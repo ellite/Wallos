98e97ef08db41de9ae09597c1afd21ffdb2c0e09
ellite <k.d.mitnick@gmail.com>
2023-11-16 21:40:30 +0100

    Make DB migrator work for both docker and baremetal

--- endpoints/db/migrate.php
+++ endpoints/db/migrate.php
@@ -1,6 +1,20 @@
 <?php
+function errorHandler($severity, $message, $file, $line) {
+    throw new ErrorException($message, 0, $severity, $file, $line);
+}
+
+// Set the custom error handler
+set_error_handler('errorHandler');
 /** @var \SQLite3 $db */
-require_once 'includes/connect.php';
+try {
+    require_once 'includes/connect_endpoint_crontabs.php';
+} catch (Exception $e) {
+    require_once '../../includes/connect_endpoint.php';
+} finally {
+    // Restore the default error handler
+    restore_error_handler();
+}
+
 
 $completedMigrations = [];
 
