f7b0116247b866e471137a907af370ff41b6fc94
Matt Bannon <git@ttam.co>
2023-11-16 22:31:02 +1100

    Check for column existence in migration Allow migrations from bare installations

--- migrations/000002.php
+++ migrations/000002.php
@@ -3,5 +3,10 @@
 // It allows the user to disable payment methods without deleting them.
 
 /** @noinspection PhpUndefinedVariableInspection */
-$db->exec('ALTER TABLE payment_methods ADD COLUMN enabled BOOLEAN DEFAULT 1');
-$db->exec('UPDATE payment_methods SET enabled = 1');
+$columnQuery = $db->query("SELECT * FROM pragma_table_info('payment_methods') where name='enabled'");
+$columnRequired = $columnQuery->fetchArray(SQLITE3_ASSOC) === false;
+
+if ($columnRequired) {
+    $db->exec('ALTER TABLE payment_methods ADD COLUMN enabled BOOLEAN DEFAULT 1');
+    $db->exec('UPDATE payment_methods SET enabled = 1');
+}
