c545dbcfcb764d8188230fe1472a3ae8161f27c3
Joke Frerichs <post@jokefrerichs.de>
2024-01-26 20:49:42 +0100

    first iteration on german translation

---
+++ scripts/i18n/de.js
@@ -0,0 +1,36 @@
+let i18n = {
+  // Dashboard
+  error_reloading_subscription: "Fehler beim Aktualisieren des Abonnements:",
+  error_fetching_image_results: "Fehler beim Laden der Bilder:",
+  subscription_deleted: "Abonnement gelöscht",
+  error_deleting_subscription: "Fehler beim Löschen des Abonnements",
+  failed_to_load_subscription: "Fehler beim Laden des Abonnements",
+  edit_subscription: "Abonnement bearbeiten",
+  add_subscription: "Abonnement hinzufügen",
+  // Settings
+  network_response_error: "Netzwerkfehler",
+  failed_add_member: "Hinzufügen von Mitglied fehlgeschlagen",
+  member: "Mitglied",
+  save_member: "Mitglied speichern",
+  delete_member: "Mitglied löschen",
+  failed_remove_member: "Mitglied konnte nicht gelöscht werden",
+  failed_save_member: "Mitglied konnte nicht gespeichert werden",
+  failed_add_category: "Kategorie konnte nicht hinzugefügt werden",
+  category: "Kategorie",
+  save_category: "Kategorie speichern",
+  delete_category: "Kategorie löschen",
+  failed_remove_category: "Kategorie konnte nicht gelöscht werden",
+  currency: "Währung",
+  currency_code: "Währungscode",
+  save_currency: "Währung speichern",
+  delete_currency: "Währung löschen",
+  failed_remove_currency: "Währung konnte nicht gelöscht werden",
+  failed_save_currency: "Währung konnte nicht gespeichert werden",
+  cant_disable_payment_in_use:
+    "Genutzte Währungen können nicht deaktiviert werden",
+  failed_save_payment_method: "Zahlungsmethode konnte nicht gespeichert werden",
+  unknown_error: "Unbekannter Fehler, bitte erneut versuchen.",
+  error_saving_notification_data:
+    "Fehler beim Speichern der Benachrichtigungsangaben",
+  error_sending_notification: "Fehler beim Senden der Benachrichtigung",
+}
