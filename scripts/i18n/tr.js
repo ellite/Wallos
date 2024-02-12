90009cf5c66d75cf58f386eda8aa876c6999a4ca
Talha Keskin <72507373+raymenstereo@users.noreply.github.com>
2024-02-12 22:10:20 +0100

    Added Türkce/turkish (#109)

---
+++ scripts/i18n/tr.js
@@ -0,0 +1,37 @@
+let i18n = {
+  // Dashboard
+  error_reloading_subscription: "Abonelik yeniden yüklenirken hata oluştu:",
+  error_fetching_image_results: "Görüntü sonuçları alınırken hata oluştu:",
+  subscription_deleted: "Abonelik silindi",
+  error_deleting_subscription: "Abonelik silinirken hata oluştu",
+  failed_to_load_subscription: "Abonelik yüklenemedi",
+  edit_subscription: "Aboneliği Düzenle",
+  add_subscription: "Abonelik Ekle",
+  // Ayarlar
+  network_response_error: "Ağ yanıtı kabul edilmedi",
+  failed_add_member: "Üye eklenemedi",
+  member: "Üye",
+  save_member: "Üyeyi Kaydet",
+  delete_member: "Üyeyi Sil",
+  failed_remove_member: "Üye silinmedi",
+  failed_save_member: "Üye kaydedilemedi",
+  failed_add_category: "Kategori eklenemedi",
+  category: "Kategori",
+  save_category: "Kategoriyi Kaydet",
+  delete_category: "Kategoriyi Sil",
+  failed_remove_category: "Kategori silinmedi",
+  currency: "Para Birimi",
+  currency_code: "Para Birimi Kodu",
+  save_currency: "Para Birimini Kaydet",
+  delete_currency: "Para Birimini Sil",
+  failed_remove_currency: "Para birimi kaldırılamadı",
+  failed_save_currency: "Para birimi kaydedilemedi",
+  cant_disable_payment_in_use: "Kullanımdaki ödemeyi devre dışı bırakamazsınız",
+  failed_save_payment_method: "Ödeme yöntemi kaydedilemedi",
+  unknown_error: "Bilinmeyen hata, lütfen tekrar deneyin.",
+  error_saving_notification_data: "Bildirim verisi kaydedilirken hata oluştu",
+  error_sending_notification: "Bildirim gönderilirken hata oluştu"
+}
+
+
+
