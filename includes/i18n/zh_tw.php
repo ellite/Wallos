4938b16e025b238ad3d0cce224ee6c00483598d4
Peter Dave Hello <hsu@peterdavehello.org>
2024-02-18 13:37:31 +0800

    Improve zh_TW translation

--- includes/i18n/zh_tw.php
+++ includes/i18n/zh_tw.php
@@ -2,7 +2,7 @@
 
 $i18n = [
     // 註冊頁面
-    "create_account"  => "請先建立帳號再登錄",
+    "create_account"  => "請先建立帳號再登入",
     'username'        => "使用者名稱",
     'password'        => "密碼",
     "email"           => "電子信箱",
@@ -12,20 +12,17 @@ $i18n = [
     "passwords_dont_match" => "密碼不一致",
     "registration_failed" => "註冊失敗，請再試一次。",
     "register"        => "註冊",
-
-    // 登錄頁面
-    'please_login'    => "請先登錄",
-    'stay_logged_in'  => "保持登錄 30 天",
-    'login'           => "登錄",
-    'login_failed'    => "登錄資訊錯誤",
-
-    // 頁眉
+    // 登入頁面
+    'please_login'    => "請先登入",
+    'stay_logged_in'  => "保持登入 30 天",
+    'login'           => "登入",
+    'login_failed'    => "登入資訊錯誤",
+    // 頁首
     'subscriptions'   => "訂閱",
     'stats'           => "統計",
     'settings'        => "設定",
     'about'           => "關於",
     'logout'          => "登出",
-
     // 訂閱頁面
     "subscription"    => "訂閱",
     "no_subscriptions_yet" => "目前還沒有任何訂閱",
@@ -48,15 +45,14 @@ $i18n = [
     "weeks"           => "週",
     "months"          => "月",
     "years"           => "年",
-    "external_url"    => "查看外部連結",
+    "external_url"    => "檢視外部連結",
     "empty_page"      => "空白頁面",
-    
     // 訂閱表單
     "add_subscription" => "新增訂閱",
     "edit_subscription" => "編輯訂閱",
     "subscription_name" => "訂閱名稱",
-    "logo_preview"    => "圖標預覽",
-    "search_logo"     => "在網路上搜尋圖標",
+    "logo_preview"    => "圖示預覽",
+    "search_logo"     => "在網路上搜尋圖示",
     "web_search"      => "網頁搜尋",
     "currency"        => "貨幣",
     "billing_cycle"   => "帳單週期",
@@ -71,8 +67,7 @@ $i18n = [
     "enable_notifications" => "為該訂閱開啟通知",
     "delete"          => "刪除",
     "cancel"          => "取消",
-    "upload_logo"     => "上傳圖標",
-    
+    "upload_logo"     => "上傳圖示",
     // 統計頁面
     'general_statistics' => "整體統計",
     'active_subscriptions' => "訂閱中",
@@ -83,23 +78,21 @@ $i18n = [
     'most_expensive'  => "最高的訂閱費用",
     'amount_due'      => "本月應付金額",
     'monthly_savings' => "每月節省",
-    'split_views'     => "分割表示",
-    'category_split'  => "類別表示",
-    'household_split' => "家庭表示",
-    'payment_method_split' => "付款方式表示",
-    
+    'split_views'     => "分割檢視",
+    'category_split'  => "類別分割",
+    'household_split' => "家庭分割",
+    'payment_method_split' => "付款方式分割",
     // 關於頁面
     'about_and_credits' => "關於和致謝",
     'license'         => "授權許可",
     'issues_and_requests' => "問題回饋與功能請求",
     'the_author'      => "作者",
-    'icons'           => "圖標",
-    'payment_icons'   => "付款圖標",
-    
+    'icons'           => "圖示",
+    'payment_icons'   => "付款圖示",
     // 設定頁面
-    'user_details'    => "用戶詳情",
+    'user_details'    => "使用者詳細資訊",
     "household"       => "家庭",
-    "save_member"     => "保存成員",
+    "save_member"     => "儲存成員",
     "delete_member"   => "刪除成員",
     "cant_delete_member" => "無法刪除主要成員",
     "cant_delete_member_in_use" => "無法刪除擁有訂閱的成員",
@@ -116,105 +109,97 @@ $i18n = [
     "from_email"      => "寄件人信箱（可選）",
     "smtp_info"       => "SMTP 密碼將以明文傳輸和儲存。為了安全起見，建議專門為 Wallos 建立一個帳戶。",
     "categories"      => "分類",
-    "save_category"   => "保存分類",
+    "save_category"   => "儲存分類",
     "delete_category" => "刪除分類",
     "cant_delete_category_in_use" => "無法刪除正在訂閱中的分類",
     "currencies"      => "貨幣",
-    "save_currency"   => "保存貨幣",
+    "save_currency"   => "儲存貨幣",
     "delete_currency" => "刪除貨幣",
     "cant_delete_main_currency" => "無法刪除主要貨幣",
     "cant_delete_currency_in_use" => "無法刪除正在使用中的貨幣",
     "exchange_update" => "匯率最後更新於",
     "currency_info"   => "如要查詢支援的貨幣與相對應的貨幣代碼，請前往",
-    "currency_performance" => "為提高性能，建議您只保留常用的貨幣。",
-    "fixer_api_key"   => "Fixer API 密鑰",
-    "api_key"         => "API 密鑰",
+    "currency_performance" => "為提高效能，建議您只保留常用的貨幣。",
+    "fixer_api_key"   => "Fixer API 金鑰",
+    "api_key"         => "API 金鑰",
     "provider"        => "提供者",
-    "fixer_info"      => "如果您使用多種貨幣單位，且希望統計資訊和訂閱排序更加精確，則需要 Fixer API 密鑰來查詢匯率（可免費申請）。",
-    "get_key"         => "申請密鑰",
-    "get_free_fixer_api_key" => "申請免費的 Fixer API 密鑰",
+    "fixer_info"      => "如果您使用多種貨幣單位，且希望統計資訊和訂閱排序更加精確，則需要 Fixer API 金鑰來查詢匯率（可免費申請）。",
+    "get_key"         => "申請金鑰",
+    "get_free_fixer_api_key" => "申請免費的 Fixer API 金鑰",
     "get_key_alternative" => "或者，您可以從以下網址取得一個免費的修復 api 金鑰",
     "display_settings" => "顯示設定",
     "switch_theme"    => "切換淺色/深色主題",
     "calculate_monthly_price" => "計算並顯示所有訂閱的每月價格",
     "convert_prices"  => "始終按照我的主要貨幣單位轉換和顯示價格（較慢）",
     "experimental_settings" => "實驗性設定",
-    "remove_background" => "嘗試從圖片搜尋中移除圖標的背景顏色（實驗性）",
+    "remove_background" => "嘗試從圖片搜尋中移除圖示的背景顏色（實驗性）",
     "experimental_info" => "實驗性設定，可能存在問題。",
     "payment_methods" => "付款方式",
-    "payment_methods_info" => "點擊付款方式以停用/啟用。",
+    "payment_methods_info" => "點選付款方式以停用/啟用。",
     "cant_delete_payment_method_in_use" => "無法停用正在使用的付款方式",
     "disable"         => "停用",
     "enable"          => "啟用",
     "test"            => "測試",
     "add"             => "新增",
-    "save"            => "保存",
-    "export_subscriptions" => "導出訂閱",
-    "export_to_json"  => "導出為 JSON 檔案",
-    
+    "save"            => "儲存",
+    "export_subscriptions" => "匯出訂閱",
+    "export_to_json"  => "匯出為 JSON 檔案",
     // Toast
     "success"         => "成功",
-    
     // Endpoint responses
-    "session_expired" => "目前的工作階段已經過期，請重新登錄",
+    "session_expired" => "目前的工作階段已經過期，請重新登入",
     "fields_missing" => "部分欄位未填寫",
     "fill_all_fields" => "請填寫所有欄位",
     "fill_mandatory_fields" => "請填寫所有必填欄位",
     "error" => "錯誤",
-    
     // Category
     "failed_add_category" => "新增分類失敗",
     "failed_edit_category" => "編輯分類失敗",
     "category_in_use" => "分類正在被訂閱使用中，無法移除",
     "failed_remove_category" => "移除分類失敗",
-    "category_saved" => "分類已保存",
+    "category_saved" => "分類已儲存",
     "category_removed" => "分類已移除",
-    
     // Currency
-    "currency_saved" => "已保存。",
+    "currency_saved" => "已儲存。",
     "error_adding_currency" => "新增貨幣時發生錯誤。",
     "failed_to_store_currency" => "儲存貨幣到資料庫失敗。",
     "currency_in_use" => "貨幣正在被訂閱使用中，無法刪除。",
     "currency_is_main" => "貨幣已被設定為主貨幣，無法刪除。",
     "failed_to_remove_currency" => "從資料庫中刪除貨幣失敗。",
-    "failed_to_store_api_key" => "儲存 API 密鑰到資料庫失敗。",
-    "invalid_api_key" => "API 密鑰無效。",
-    "api_key_saved" => "API 密鑰已成功保存",
+    "failed_to_store_api_key" => "儲存 API 金鑰到資料庫失敗。",
+    "invalid_api_key" => "API 金鑰無效。",
+    "api_key_saved" => "API 金鑰已成功儲存",
     "currency_removed" => "貨幣已移除",
-    
     // Household
     "failed_add_household" => "新增家庭成員失敗",
     "failed_edit_household" => "編輯家庭成員失敗",
     "failed_remove_household" => "移除家庭成員失敗",
     "household_in_use" => "此成員有相關的訂閱，無法移除",
-    "member_saved" => "成員已保存",
+    "member_saved" => "成員已儲存",
     "member_removed" => "成員已移除",
-    
     // Notifications
-    "error_saving_notifications" => "保存通知資料時發生錯誤。",
+    "error_saving_notifications" => "儲存通知資料時發生錯誤。",
     "wallos_notification" => "Wallos 通知",
-    "test_notification" => "這是一條測試用的通知。如果您看到此消息，說明 Wallos 通知信箱功能設定正確。",
-    "email_error" => "發送到電子信箱時發生錯誤",
-    "notification_sent_successfuly" => "通知已成功發送",
-    "notifications_settings_saved" => "通知設定已成功保存。",
-    
+    "test_notification" => "這是一條測試用的通知。如果您看到此訊息，說明 Wallos 通知信箱功能設定正確。",
+    "email_error" => "傳送到電子信箱時發生錯誤",
+    "notification_sent_successfuly" => "通知已成功傳送",
+    "notifications_settings_saved" => "通知設定已成功儲存。",
     // Payments
     "payment_in_use" => "無法停用正在使用的付款方式",
     "failed_update_payment" => "更新資料庫中的付款方式失敗",
     "enabled" => "已啟用",
     "disabled" => "已停用",
-    
     // Subscription
     "error_fetching_image" => "讀取圖片時發生錯誤",
     "subscription_updated_successfuly" => "訂閱已成功更新",
     "subscription_added_successfuly" => "訂閱已成功新增",
     "error_deleting_subscription" => "刪除訂閱時發生錯誤。",
     "invalid_request_method" => "請求方法無效。",
-    
     // User
-    "error_updating_user_data" => "更新用戶資料時發生錯誤。",
-    "user_details_saved" => "用戶詳細訊息已保存",
+    "error_updating_user_data" => "更新使用者資訊時發生錯誤。",
+    "user_details_saved" => "使用者資訊已儲存",
 
 ];
+
 
 ?>
