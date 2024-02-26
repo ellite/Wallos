5ccadce2f139e5873889badc51a67bfaef8a9304
Miguel Ribeiro <k.d.mitnick@gmail.com>
2024-02-27 00:02:13 +0100

    fix: remove service worker (#157)

--- includes/header.php
+++ includes/header.php
@@ -38,7 +38,6 @@
   <link rel="stylesheet" href="styles/dark-theme.css?<?= $version ?>" id="dark-theme" <?= $theme == "light" ? "disabled" : "" ?>>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Barlow:300,400,500,600,700">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-  <script type="text/javascript" src="scripts/all.js?<?= $version ?>"></script>
   <script type="text/javascript" src="scripts/common.js?<?= $version ?>"></script>
   <script type="text/javascript">
     window.theme = "<?= $theme ?>";
