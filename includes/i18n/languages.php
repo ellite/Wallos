2c186b48181bab97c8eed7a0036a48296320376f
Luca <luca.cirillo5@gmail.com>
2024-04-19 10:27:59 +0200

    Italian translation (#280)

--- includes/i18n/languages.php
+++ includes/i18n/languages.php
@@ -1,6 +1,6 @@
 <?php
-    // File Name => Language Name
 
+    // File Name => Language Name
     $languages = [
         // English first
         "en" => "English",
@@ -9,12 +9,13 @@
         "el" => "Ελληνικά",
         "es" => "Español",
         "fr" => "Français",
+        "it" => "Italiano",
         "jp" => "日本語",
-	"pl" => "Polski",
-        "pt" => "Português",
+        "pl" => "Polski",
         "pt_br" => "Português Brasileiro",
-	"sr" => "Српски",
-	"sr_lat" => "Srpski",
+        "pt" => "Português",
+        "sr_lat" => "Srpski",
+        "sr" => "Српски",
         "tr" => "Türkçe",
         "zh_cn" => "简体中文",
         "zh_tw" => "繁體中文",
