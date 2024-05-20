a8ace8e68702d06a8213d6b0ec4b271c6af0794b
Miguel Ribeiro <k.d.mitnick@gmail.com>
2024-05-20 14:27:51 +0200

    fix get subscription endpoint

--- endpoints/subscription/get.php
+++ endpoints/subscription/get.php
@@ -13,20 +13,20 @@
 
             if ($row = $result->fetchArray(SQLITE3_ASSOC)) {
                 $subscriptionData['id'] = $subscriptionId;
-                $subscriptionData['name'] = htmlspecialchars_decode($row['name']);
+                $subscriptionData['name'] = htmlspecialchars_decode($row['name'] ?? "");
                 $subscriptionData['logo'] = $row['logo'];
                 $subscriptionData['price'] = $row['price'];
                 $subscriptionData['currency_id'] = $row['currency_id'];
                 $subscriptionData['next_payment'] = $row['next_payment'];
                 $subscriptionData['frequency'] = $row['frequency'];
                 $subscriptionData['cycle'] = $row['cycle'];
-                $subscriptionData['notes'] = htmlspecialchars_decode($row['notes']);
+                $subscriptionData['notes'] = htmlspecialchars_decode($row['notes'] ?? "");
                 $subscriptionData['payment_method_id'] = $row['payment_method_id'];
                 $subscriptionData['payer_user_id'] = $row['payer_user_id'];
                 $subscriptionData['category_id'] = $row['category_id'];
                 $subscriptionData['notify'] = $row['notify'];
                 $subscriptionData['inactive'] = $row['inactive'];
-                $subscriptionData['url'] = htmlspecialchars_decode($row['url']);
+                $subscriptionData['url'] = htmlspecialchars_decode($row['url'] ?? "");
                 $subscriptionData['notify_days_before'] = $row['notify_days_before'];
 
                 $subscriptionJson = json_encode($subscriptionData);
