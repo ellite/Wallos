384bfd3798fd36eab8e68a65cce2aef1bb0a3f1c
Miguel Ribeiro <k.d.mintnick@gmail.com>
2025-01-12 00:34:08 +0100

    feat: new api endpoint that returns the version

---
+++ api/status/version.php
@@ -0,0 +1,81 @@
+<?php
+/*
+This API Endpoint accepts both POST and GET requests.
+It receives the following parameters:
+- apiKey: the API key of the user.
+
+It returns a JSON object with the following properties:
+- success: whether the request was successful (boolean).
+- title: the title of the response (string).
+- version: a string containing the version matching the github package version.
+- version_number: a string containing the version number.
+- notes: warning messages or additional information (array).
+
+Example response:
+{
+  "success": true,
+  "title": "version",
+  "version": "v2.42.1",
+  "version_number": "2.42.1",
+  "notes": []
+}
+*/
+
+require_once '../../includes/connect_endpoint.php';
+require_once '../../includes/version.php';
+
+header('Content-Type: application/json, charset=UTF-8');
+
+if ($_SERVER["REQUEST_METHOD"] === "POST" || $_SERVER["REQUEST_METHOD"] === "GET") {
+    // if the parameters are not set, return an error
+
+    if (!isset($_REQUEST['api_key'])) {
+        $response = [
+            "success" => false,
+            "title" => "Missing parameters"
+        ];
+        echo json_encode($response);
+        exit;
+    }
+
+    $apiKey = $_REQUEST['api_key'];
+
+    // Get user from API key
+    $sql = "SELECT * FROM user WHERE api_key = :apiKey";
+    $stmt = $db->prepare($sql);
+    $stmt->bindValue(':apiKey', $apiKey);
+    $result = $stmt->execute();
+    $user = $result->fetchArray(SQLITE3_ASSOC);
+
+    // If the user is not found, return an error
+    if (!$user) {
+        $response = [
+            "success" => false,
+            "title" => "Invalid API key"
+        ];
+        echo json_encode($response);
+        exit;
+    }
+
+    $version_number = substr($version, 1);
+
+    $response = [
+        "success" => true,
+        "title" => "version",
+        "version" => $version,
+        "version_number" => $version_number,
+        "notes" => []
+    ];
+
+    echo json_encode($response);
+
+} else {
+    $response = [
+        "success" => false,
+        "title" => "Invalid request method"
+    ];
+    echo json_encode($response);
+    exit;
+}
+
+?>
