69c7d52cf8d708bcb046343faa663209c8d36779
Miguel Ribeiro <developer@henrique.pt>
2025-05-03 15:37:10 +0200

    fix: vulnerability on delete avatar
    fix: some strings not using translations on the calendar page
    fix: delete avatar would not work if wallos is on a subfolder

--- scripts/profile.js
+++ scripts/profile.js
@@ -77,7 +77,7 @@ function successfulUpload(field, msg) {
 }
 
 function deleteAvatar(path) {
-    fetch('/endpoints/user/delete_avatar.php', {
+    fetch('endpoints/user/delete_avatar.php', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -93,7 +93,7 @@ function deleteAvatar(path) {
                 }
                 showSuccessMessage();
             } else {
-                showErrorMessage();
+                showErrorMessage(data.message || "");
             }
         })
         .catch((error) => {
